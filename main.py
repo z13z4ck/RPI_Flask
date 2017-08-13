1d93b38a4454b1fe6276efefe4a8d093911089d2
Aziz <z13z4ck@gmail.com>
2017-08-13 23:37:16 +0800

    fix address issue
    minor fix on the address

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import *
 import rpi
 
 
@@ -20,7 +20,7 @@ def index():
 
 
 # change gpio value
-@app.route("/toggle_gpio_state/int:status>", methods=['POST'])
+@app.route("/toggle_gpio_state/<int:status>", methods=['POST'])
 def toggle_gpio_status(status):
     # check the value of the parameter
     if not status:
@@ -40,4 +40,4 @@ def about():
     return render_template('about.html')
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", debug=True)
+    app.run(host="0.0.0.0", debug=True)
